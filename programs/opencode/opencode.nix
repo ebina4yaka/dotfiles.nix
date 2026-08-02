12ca8c5d811054dc441ccf6f0a12d1da2d2c4179
ebina4yaka <ebina4yaka@protonmail.com>
2026-08-02 05:04:27 +0000

    opencode themeを修正

--- programs/opencode/opencode.nix
+++ programs/opencode/opencode.nix
@@ -5,17 +5,7 @@
     ".config/opencode/tui.json".text = ''
       {
         "$schema": "https://opencode.ai/tui.json",
-        "theme": "transparent"
-      }
-    '';
-    ".config/opencode/themes/transparent.json".text = ''
-      {
-        "theme": {
-          "background": "none",
-          "backgroundPanel": "none",
-          "backgroundElement": "none",
-          "text": "none"
-        }
+        "theme": "system"
       }
     '';
   };
