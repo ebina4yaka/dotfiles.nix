65f004a2a6193c00868de73c5f652eb5b9dd7795
ebina4yaka <ebina4yaka@protonmail.com>
2026-09-20 21:46:54 +0000

    modify emacs

--- programs/emacs/doom/config.el
+++ programs/emacs/doom/config.el
@@ -7,12 +7,41 @@
 ;; ツリーを右に出す。
 (setq treemacs-position 'right)      ; nixvim: neo-tree の window.position
 
+;; emacs . のようにディレクトリを開いたら treemacs も出す。既に出ているときは
+;; 何もしない。フォーカスは dired 側に残す。
+(defun +treemacs-ensure-visible ()
+  "treemacs が隠れていれば現在のプロジェクトを表示する。"
+  (require 'treemacs)
+  (unless (eq (treemacs-current-visibility) 'visible)
+    (save-selected-window (+treemacs/toggle))))
+(add-hook 'dired-mode-hook #'+treemacs-ensure-visible)
+
 ;; eglot は nix のサーバーを知らないので登録する。nixvim と同じ nixd。
 (after! eglot
   (set-eglot-client! '(nix-mode nix-ts-mode) '("nixd")))
 
 ;; ここから下は nixvim の keymaps との差分だけを書く。
 ;; gd / gr / K / SPC g g は Doom 既定が nixvim と一致するので触らない。
+
+;; nixvim は n / t モードの <C-hjkl> を <C-w>hjkl に割り当てている。
+(map! :n "C-h" #'evil-window-left
+      :n "C-j" #'evil-window-down
+      :n "C-k" #'evil-window-up
+      :n "C-l" #'evil-window-right)
+;; ターミナル入力中（nixvim の t モード相当）と treemacs のバッファでも同じ
+;; キーでウィンドウを移動できるようにする。
+(map! (:after vterm
+       :map vterm-mode-map
+       "C-h" #'evil-window-left
+       "C-j" #'evil-window-down
+       "C-k" #'evil-window-up
+       "C-l" #'evil-window-right)
+      (:after treemacs-evil
+       :map evil-treemacs-state-map
+       "C-h" #'evil-window-left
+       "C-j" #'evil-window-down
+       "C-k" #'evil-window-up
+       "C-l" #'evil-window-right))
 
 (map! :n "gn" #'eglot-rename                    ; nixvim: gn（evil の gn を潰す）
       :n "ga" #'eglot-code-actions              ; nixvim: ga（evil の ga を潰す）
